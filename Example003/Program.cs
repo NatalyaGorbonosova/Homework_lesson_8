e70b61ecb31370ca56f43a6894b4477d71261260
NatalyaGorbonosova <gorbonosova.n@mail.ru>
2022-09-14 21:56:27 +0300

    Task Matrix3D

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+void FillMatrix(int[,] table)
+{
+    for(int i=0; i < table.GetLength(0); i++)
+    {
+        for(int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintMatrix(int[,] table)
+{
+    for(int i=0; i < table.GetLength(0); i++)
+    {
+        for(int j=0; j < table.GetLength(1); j++)
+        {
+            Console.Write(table[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] MultiplicationMatrix(int[,] tableFirst, int[,] tableSecond)
+{
+    int[,] mult = new int[tableFirst.GetLength(0), tableSecond.GetLength(1)];
+    int[] arrFirstRows = new int[tableFirst.GetLength(1)];
+    int[] arrSecondColums = new int[tableSecond.GetLength(0)];
+    for(int i = 0; i < tableFirst.GetLength(0); i++)
+    {
+        for(int j = 0; j < tableFirst.GetLength(1); j++)
+        {
+            arrFirstRows[j] = tableFirst[i, j];
+        }
+        for(int j1 = 0; j1 < tableSecond.GetLength(1); j1++)
+        {
+            int sum = 0;
+            for(int i1 = 0; i1 < tableSecond.GetLength(0); i1++)
+            {
+                arrSecondColums[i1] = tableSecond[i1, j1];
+                sum += arrFirstRows[i1] * arrSecondColums[i1];
+            }
+            mult[i, j1] = sum;
+        }
+    }
+    return mult;
+}
+try
+{
+     Console.WriteLine("Введите размерность первой матрицы");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int m = Convert.ToInt32(Console.ReadLine());
+    int[,] matrixFirst = new int[n, m];
+    FillMatrix(matrixFirst);
+    PrintMatrix(matrixFirst);
+    Console.WriteLine();
+     Console.WriteLine("Введите размерность второй матрицы");
+    int k = Convert.ToInt32(Console.ReadLine());
+    int l = Convert.ToInt32(Console.ReadLine());
+    int[,] matrixSecond = new int[k, l];
+    FillMatrix(matrixSecond);
+    PrintMatrix(matrixSecond);
+    Console.WriteLine();
+    if(m == k)
+    {
+        Console.WriteLine("Произведение матриц равно");
+        int[,] multMatrix = MultiplicationMatrix(matrixFirst, matrixSecond);
+        PrintMatrix(multMatrix);
+    }
+    else Console.WriteLine("Эти матрицы умножить нельзя");
+    
+ }
+catch
+{
+    Console.WriteLine("Вводите только числа");
+}
---
+++ Example004/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+void FillMatrix3D(int[,,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for(int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = new Random().Next(10, 100);
+            }
+        }
+    }
+}
+void PrintMatrix3D(int[,,] matrix)
+{
+     for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for(int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write(matrix[i, j, k] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+void PrintMatrix3DWithIndex(int[,,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for(int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}  ({i}, {j}, {k}){"\t"}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+try
+{
+    Console.WriteLine("Введите размерность матрицы");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int m = Convert.ToInt32(Console.ReadLine());
+    int l = Convert.ToInt32(Console.ReadLine());
+    int[,,] matrix = new int[n, m, l];
+    FillMatrix3D(matrix);
+    PrintMatrix3D(matrix);
+    Console.WriteLine();
+    PrintMatrix3DWithIndex(matrix);
+    
+ }
+catch
+{
+    Console.WriteLine("Вводите только числа");
+}
