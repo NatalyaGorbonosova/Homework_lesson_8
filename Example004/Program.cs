9242c03d3bd8ccdbf14274dd31aacd4b93c4a9b1
NatalyaGorbonosova <gorbonosova.n@mail.ru>
2022-09-16 21:41:15 +0300

    Task  Matrix

--- Example004/Program.cs
+++ Example004/Program.cs
@@ -1,14 +1,32 @@
 ﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-void FillMatrix3D(int[,,] matrix)
+void FillMatrix3DRandom(int[,,] matrix)
 {
+    int[] arr = new int[matrix.GetLength(0) * 
+                            matrix.GetLength(1) *
+                                matrix.GetLength(2)];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        var num = new Random().Next(10, 100);
+ 
+        if (arr.Contains(num))
+        {
+            i--;
+        }
+        else
+        {
+                    arr[i] = num;
+        }
+    }
+    int number = 0;
     for(int i = 0; i < matrix.GetLength(0); i++)
     {
         for(int j = 0; j < matrix.GetLength(1); j++)
         {
             for(int k = 0; k < matrix.GetLength(2); k++)
             {
-                matrix[i, j, k] = new Random().Next(10, 100);
+                matrix[i, j, k] = arr[number];
+                number++;
             }
         }
     }
@@ -48,11 +66,10 @@ try
     int m = Convert.ToInt32(Console.ReadLine());
     int l = Convert.ToInt32(Console.ReadLine());
     int[,,] matrix = new int[n, m, l];
-    FillMatrix3D(matrix);
+    FillMatrix3DRandom(matrix);
     PrintMatrix3D(matrix);
     Console.WriteLine();
     PrintMatrix3DWithIndex(matrix);
-    
  }
 catch
 {
---
+++ Example005/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+void FillMatrixSpirally(int[,] table)
+{
+    int n = table.GetLength(0);
+    int number = 1;
+    for(int k = 0; k < (n + 1)/2; k++)
+    {
+        for(int j = k; j < n - k; j++)
+        {
+            table[k, j] = number;
+            number += 1;
+        }
+        for(int i = k + 1; i < n - k; i++)
+        {
+            table[i, n - 1 - k] = number;
+            number += 1;
+         }
+        for(int j = n - k - 2; j >= k; j--)
+        {
+            table[n - k - 1, j] = number;
+            number += 1;
+        }
+        for(int i = n - k - 2; i > k; i--)
+        {
+            table[i, k] = number;
+            number += 1;
+        }
+        
+    }
+}
+void PrintMatrix(int[,] table)
+{
+    for(int i=0; i < table.GetLength(0); i++)
+    {
+        for(int j=0; j < table.GetLength(1); j++)
+        {
+            Console.Write("{0}\t" + table[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+try
+{
+    Console.WriteLine("Введите размерность квадратной матрицы");
+    int N = Convert.ToInt32(Console.ReadLine());
+    int[,] matrix = new int[N, N];
+    FillMatrixSpirally(matrix);
+    PrintMatrix(matrix);
+}
+catch
+{
+    Console.WriteLine("Вводите числа");
+}
